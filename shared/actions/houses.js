a2f7dc9bb912ae14245b2780477dd8ed7e53c145
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-24 11:32:06 -0600

    Message Skokie

---
+++ shared/actions/houses.js
@@ -0,0 +1,41 @@
+//axios - Promise based HTTP client for the browser and node.js
+import request from 'axios';
+
+export const HOUSES_GET = 'HOUSES_GET';
+export const HOUSES_GET_REQUEST = 'HOUSES_GET_REQUEST';
+export const HOUSES_GET_SUCCESS = 'HOUSES_GET_SUCCESS';
+export const HOUSES_GET_FAILURE = 'HOUSES_GET_FAILURE';
+
+export const ADD_HOUSE = 'ADD_HOUSE';
+export const REMOVE_HOUSE = 'REMOVE_HOUSE';
+
+/*Action Creator functions*/
+export function housesGet() {
+    return {
+        type: HOUSES_GET,
+        promise: request.get('https://real-estate-react.firebaseio.com/sale/skokie.json')
+    };
+}
+
+export function addHouse(house) {
+    return {
+        type: ADD_HOUSE,
+        payload: house
+    };
+}
+export function removeHouse(index) {
+    return {
+        type: REMOVE_HOUSE,
+        payload: index
+    };
+}
+
+export function getHousesIfNeeded() {
+    return (dispatch, getState) => {
+        if (!getState().houses.length) {
+            return dispatch(housesGet());
+        }
+    };
+}
+
+
