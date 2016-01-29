094e7cce255cd45d7e4f1b3772d348b9d6eccbac
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-29 12:37:53 -0600

    Fix errors with Breadcrumbs

--- shared/actions/cityInfo.js
+++ shared/actions/cityInfo.js
@@ -1,5 +1,6 @@
 //axios - Promise based HTTP client for the browser and node.js
 import request from 'axios';
+import config from '../config';
 
 export const CITYINFO_GET = 'CITYINFO_GET';
 export const CITYINFO_GET_REQUEST = 'CITYINFO_GET_REQUEST';
@@ -7,11 +8,11 @@ export const CITYINFO_GET_SUCCESS = 'CITYINFO_GET_SUCCESS';
 export const CITYINFO_GET_FAILURE = 'CITYINFO_GET_FAILURE';
 
 /*Action Creator functions*/
-export function cityInfoGet(saleRent,city) {
+export function cityInfoGet(saleRent, city) {
     return {
         type: CITYINFO_GET,
-        promise: request.get(`https://real-estate-react.firebaseio.com/${saleRent}/${city}.json`),
-        obj:true
+        promise: request.get(`${config.fireDb}${saleRent}/${city}.json`),
+        obj: true
     };
 }
 
@@ -19,7 +20,7 @@ export function getCityInfoIfNeeded(params, location) {
     const saleRent = location.pathname.indexOf('sale') > -1 ? 'sale' : 'rent';
     const city = params.city;
     return (dispatch, getState) => {
-            return dispatch(cityInfoGet(saleRent,city));
+        return dispatch(cityInfoGet(saleRent, city));
 
     };
 }
